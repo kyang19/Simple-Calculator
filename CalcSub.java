ca72075264a7be43c3afcb7627dd7e5e74f7bc8f
kyang19 <42977150+kyang19@users.noreply.github.com>
2018-09-17 20:57:08 -0400

    Add files via upload

---
+++ CalcSub.java
@@ -0,0 +1,16 @@
+
+public class CalcSub 
+{
+
+	public static void main(String[] args) 
+	{
+		CalcSubtract calculator = new CalcSubtract();
+		int yay = calculator.subt(25,16); 
+		int num1 = 25;
+		int num2 = 16;
+		System.out.print("The difference of " + num1 + " and " + num2 + " is " + yay
+				+ "!");
+
+	}
+
+}
---
+++ CalcSubtract.java
@@ -0,0 +1,12 @@
+
+public class CalcSubtract 
+{
+
+	public int subt(int num1, int num2)
+	{
+		int ans = num1 - num2; 
+		return ans;
+		 
+	}
+}
+ 
